86afe508e9d4c70a210210b31ecb4f696ee16591
hubjacob <61560655+hubjacob@users.noreply.github.com>
2021-04-30 14:49:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import { Router } from 'react-router-dom';
 import { history } from './utils/historyHandler';
 import Content from './components/Content';
 
-// Get type definitions on Window object for Segment event tracking
-declare global {
- 
-}
-
 function App() {
   return(
     <React.Fragment>
